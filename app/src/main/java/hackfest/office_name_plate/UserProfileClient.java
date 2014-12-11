41cbf0763e5f5f8c0cb8dd73634dac2df2292b03
mordonez <mordonez@microsoft.com>
2014-12-11 11:13:17 -0800

    app.iml update

---
+++ app/src/main/java/hackfest/office_name_plate/UserProfileClient.java
@@ -0,0 +1,20 @@
+package hackfest.office_name_plate;
+
+
+import com.microsoft.directoryservices.User;
+import com.microsoft.directoryservices.odata.DirectoryClient;
+import com.microsoft.services.odata.impl.DefaultDependencyResolver;
+
+/**
+ * Created by mordonez on 12/11/2014.
+ */
+public class UserProfileClient {
+    public static String ENDPOINT_ID = "https://outlook.office365.com/api/v1.0";
+
+    DirectoryClient directoryClient;
+
+
+}
+
+
+
